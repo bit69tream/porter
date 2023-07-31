82d611288fe1d265373aef421ee39465b4265a13
bit6tream <megapixel483@gmail.com>
2023-07-31 04:41:15 +0300

    First steps in creating a GUI for this thing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use std::path::Path;
 use image::{self, Pixel, Rgba};
 use std::env;
+use eframe::egui;
 
 fn into_intervals(bitmap: Vec<bool>) -> Vec<(usize, usize)> {
     let mut result: Vec<(usize, usize)> = Vec::new();
@@ -61,7 +64,14 @@ fn sort_image(threshold: u8, path: &str) -> Result<(), image::ImageError> {
 
 fn main() {
     let mut args: Vec<String> = env::args().skip(1).collect();
-    if args.len() < 2 {
+
+    if args.len() == 0 {
+        if gui_main().is_err() {
+            std::process::exit(1);
+        } else {
+            std::process::exit(0);
+        }
+    } else if args.len() < 2 {
         eprintln!("USAGE: porter <threshold> [images]");
         std::process::exit(1);
     }
@@ -74,4 +84,72 @@ fn main() {
             eprintln!("ERROR: Failed to sort image {}.", &path);
         }
     }
+}
+
+enum SortBy {
+    Luminance,
+    Hue,
+    Saturation
+}
+
+fn gui_main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1024.0, 1024.0)),
+        default_theme: eframe::Theme::Light,
+        resizable: false,
+        follow_system_theme: true,
+        ..Default::default()
+    };
+
+    let mut lower_threshold: u8 = 0;
+    let mut higher_threshold: u8 = 255;
+    let mut sort_by: SortBy = SortBy::Luminance;
+
+    eframe::run_simple_native("Porter", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // ui.style_mut().override_font_id = Some(egui::FontId::new(16.0, egui::FontFamily::Proportional));
+
+            ui.horizontal(|ui| {
+                ui.with_layout(egui::Layout::default().with_cross_align(egui::Align::LEFT), |ui| {
+                    ui.horizontal(|ui| {
+                        let mut new_lower_threshold: u8 = lower_threshold;
+                        ui.label("Lower threshold: ");
+                        ui.add(egui::Slider::new(&mut new_lower_threshold, 0..=255));
+                        lower_threshold = new_lower_threshold.clamp(0, higher_threshold);
+
+                        ui.separator();
+
+                        let mut new_higher_threshold: u8 = higher_threshold;
+                        ui.label("Higher threshold: ");
+                        ui.add(egui::Slider::new(&mut new_higher_threshold, 0..=255));
+                        higher_threshold = new_higher_threshold.clamp(lower_threshold, 255);
+                    });
+                });
+
+                ui.with_layout(egui::Layout::default().with_cross_align(egui::Align::RIGHT), |ui| {
+                    ui.horizontal(|ui| {
+                        let luminance_button = ui.add(egui::Button::new("Luminance"));
+                        let hue_button = ui.add(egui::Button::new("Hue"));
+                        let saturation_button = ui.add(egui::Button::new("Saturation"));
+
+                        if luminance_button.clicked() {
+                            sort_by = SortBy::Luminance;
+                        } else if hue_button.clicked() {
+                            sort_by = SortBy::Hue;
+                        } else if saturation_button.clicked() {
+                            sort_by = SortBy::Saturation;
+                        }
+
+                        match sort_by {
+                            SortBy::Luminance => luminance_button,
+                            SortBy::Hue => hue_button,
+                            SortBy::Saturation => saturation_button
+                        }.highlight();
+                    });
+                });
+            });
+        });
+    })
 }
